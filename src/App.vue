92c174b6346168d2ee3ee905fbb2fe081703a0b3
MatteoBarducci <mbardu1301@gmail.com>
2024-04-17 18:35:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+  import axios from 'axios';
+  import { store } from './store';
+  import AppSearch from './components/AppSearch.vue';
+
+  export default{
+    components: {
+      AppSearch
+    },
+    data() {
+      return {
+        store,
+      };
+    },
+    methods: {
+
+    },
+    mounted(){
+
+    }
+  }
+</script>
+
+<template>
+  <AppSearch></AppSearch>
+</template>
+
+<style scoped lang="scss">
+  @use './style/generic.scss';
+
+
+</style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,14 @@
+<script>
+    export default{
+        name: 'AppSearch',
+    }
+</script>
+
+<template>
+    ciao
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as *;
+
+</style>
